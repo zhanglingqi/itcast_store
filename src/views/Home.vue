6db0d2c178f1e2d2d10e902785b270730590ad9b
zhanglingqi <504024956@qq.com>
2018-08-09 21:51:53 +0800

    home的header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,23 @@
 <template>
 	<el-container class="container">
-		<el-header class="header">Header</el-header>
+		<!--<el-header class="header">Header</el-header>-->
+		<!-- 头部 -->
+		<el-header class="header">
+			<!-- 栅格 -->
+			<el-row>
+				<el-col :span="4">
+					<img src="@/assets/logo.png" alt="">
+				</el-col>
+				<el-col :span="19">
+					<div class="title">电商后台管理系统</div>
+				</el-col>
+				<el-col :span="1">
+					<div class="logout">
+						<a href="#">退出</a>
+					</div>
+				</el-col>
+			</el-row>
+		</el-header>
 		<el-container>
 			<el-aside class="aside" width="200px">Aside</el-aside>
 			<el-main class="main">Main</el-main>
@@ -15,12 +32,29 @@
 </script>
 
 <style>
-	+.container {
+	.container {
 		height: 100%;
 	}
 	
 	.header {
 		background-color: #b3c0d1;
+		padding: 0;
+	}
+	
+	.header .title {
+		text-align: center;
+		color: white;
+		font-size: 24px;
+		line-height: 60px;
+	}
+	
+	.header .logout {
+		line-height: 60px;
+	}
+	
+	.header .logout a {
+		color: orange;
+		text-decoration: none;
 	}
 	
 	.aside {
