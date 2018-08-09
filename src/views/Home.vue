63f7dce32f918e513b6acf25142c45cc5c931aa6
zhanglingqi <504024956@qq.com>
2018-08-09 21:44:54 +0800

    home的布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
-    Home
-  </div>
+	<el-container class="container">
+		<el-header class="header">Header</el-header>
+		<el-container>
+			<el-aside class="aside" width="200px">Aside</el-aside>
+			<el-main class="main">Main</el-main>
+		</el-container>
+	</el-container>
 </template>
 
 <script>
-export default {
+	export default {
 
-};
+	};
 </script>
 
 <style>
-
+	+.container {
+		height: 100%;
+	}
+	
+	.header {
+		background-color: #b3c0d1;
+	}
+	
+	.aside {
+		background-color: #d3dce6;
+	}
+	
+	.main {
+		background-color: #e9eef3;
+	}
 </style>
