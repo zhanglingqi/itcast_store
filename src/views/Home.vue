57a8ce0c61c31ce7c852c79cdd35c7d2c5b42450
zhanglingqi <504024956@qq.com>
2018-08-09 23:37:26 +0800

    导航菜单

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,83 +22,110 @@
 			<!--<el-aside class="aside" width="200px">Aside</el-aside>-->
 			<!--侧边栏-->
 			<el-aside class="aside" width="200px">
-				<!-- default-active 可以让某一个menu-item选中，值是menu-item的index的值 -->
-				<el-menu default-active="1-3">
+				<!-- default-active 可以让某一个menu-item选中，值是menu-item的index的值
+
+          unique-opened 默认只有一项展开
+
+          开启路由模式， menu-item的index作为路由地址
+         -->
+				<el-menu unique-opened router style="height: 100%" default-active="/users">
 					<el-submenu index="1">
 						<template slot="title">
 							<i class="el-icon-location"></i>
-							<span>导航一</span>
+							<span>用户管理</span>
 						</template>
-						<el-menu-item-group>
-							<template slot="title">分组一</template>
-							<el-menu-item index="1-1">选项1</el-menu-item>
-							<el-menu-item index="1-2">选项2</el-menu-item>
-						</el-menu-item-group>
-						<el-menu-item-group title="分组2">
-							<el-menu-item index="1-3">选项3</el-menu-item>
-						</el-menu-item-group>
-						<el-submenu index="1-4">
-							<template slot="title">选项4</template>
-							<el-menu-item index="1-4-1">选项1</el-menu-item>
-						</el-submenu>
+						<el-menu-item index="/users">
+							<i class="el-icon-menu"></i> 用户列表
+						</el-menu-item>
 					</el-submenu>
-					<el-menu-item index="2">
-						<i class="el-icon-menu"></i>
-						<span slot="title">导航二</span>
-					</el-menu-item>
-					<el-menu-item index="3" disabled>
-						<i class="el-icon-document"></i>
-						<span slot="title">导航三</span>
-					</el-menu-item>
-					<el-menu-item index="4">
-						<i class="el-icon-setting"></i>
-						<span slot="title">导航四</span>
-					</el-menu-item>
+					<el-submenu index="2">
+						<template slot="title">
+							<i class="el-icon-location"></i>
+							<span>权限管理</span>
+						</template>
+						<el-menu-item index="/roles">
+							<i class="el-icon-menu"></i> 角色列表
+						</el-menu-item>
+						<el-menu-item index="/rights">
+							<i class="el-icon-menu"></i> 权限列表
+						</el-menu-item>
+					</el-submenu>
+					<el-submenu index="3">
+						<template slot="title">
+							<i class="el-icon-location"></i>
+							<span>商品管理</span>
+						</template>
+						<el-menu-item index="3-1">
+							<i class="el-icon-menu"></i> 商品列表
+						</el-menu-item>
+						<el-menu-item index="3-2">
+							<i class="el-icon-menu"></i> 分类参数
+						</el-menu-item>
+						<el-menu-item index="3-3">
+							<i class="el-icon-menu"></i> 商品分类
+						</el-menu-item>
+					</el-submenu>
+					<el-submenu index="4">
+						<template slot="title">
+							<i class="el-icon-location"></i>
+							<span>订单管理</span>
+						</template>
+						<el-menu-item index="4-1">
+							<i class="el-icon-menu"></i> 订单列表
+						</el-menu-item>
+					</el-submenu>
+					<el-submenu index="5">
+						<template slot="title">
+							<i class="el-icon-location"></i>
+							<span>数据统计</span>
+						</template>
+						<el-menu-item index="5-1">
+							<i class="el-icon-menu"></i> 数据报表
+						</el-menu-item>
+					</el-submenu>
 				</el-menu>
 			</el-aside>
-
 			<el-main class="main">Main</el-main>
 		</el-container>
 	</el-container>
 </template>
+	<script>
+		export default {
 
-<script>
-	export default {
+		};
+	</script>
 
-	};
-</script>
-
-<style>
-	.container {
-		height: 100%;
-	}
-	
-	.header {
-		background-color: #b3c0d1;
-		padding: 0;
-	}
-	
-	.header .title {
-		text-align: center;
-		color: white;
-		font-size: 24px;
-		line-height: 60px;
-	}
-	
-	.header .logout {
-		line-height: 60px;
-	}
-	
-	.header .logout a {
-		color: orange;
-		text-decoration: none;
-	}
-	
-	.aside {
-		background-color: #d3dce6;
-	}
-	
-	.main {
-		background-color: #e9eef3;
-	}
-</style>
+	<style>
+		.container {
+			height: 100%;
+		}
+		
+		.header {
+			background-color: #b3c0d1;
+			padding: 0;
+		}
+		
+		.header .title {
+			text-align: center;
+			color: white;
+			font-size: 24px;
+			line-height: 60px;
+		}
+		
+		.header .logout {
+			line-height: 60px;
+		}
+		
+		.header .logout a {
+			color: orange;
+			text-decoration: none;
+		}
+		
+		.aside {
+			background-color: #d3dce6;
+		}
+		
+		.main {
+			background-color: #e9eef3;
+		}
+	</style>
