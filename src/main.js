43d4966d4b3d94c31653d6787ac5b8942687760c
zhanglingqi <504024956@qq.com>
2018-08-09 19:43:16 +0800

    配置登录的路由

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from './router'
 //elment 导入
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import '@/assets/css/index.css';
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false
