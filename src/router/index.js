8a3def84bf45295507546f250f4317ab1f4272bb
zhanglingqi <504024956@qq.com>
2018-08-09 21:23:50 +0800

    index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/views/Login'
+import Home from '@/views/Home';
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {name:'login',path:'/login',component:Login}
+    { name: 'Home', path: '/', component: Home },
+    { name: 'Login', path: '/login', component: Login }
   ]
 })
