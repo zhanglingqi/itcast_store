535c7c2e7485a565880a93677c6cae7ce958b9dc
zhanglingqi <504024956@qq.com>
2018-08-10 01:03:36 +0800

    面包屑和搜索文本

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -1,14 +1,40 @@
 <template>
-	<div>
-		user
-	</div>
+	<el-card class="card">
+		<!-- 面包屑组件 -->
+		<el-breadcrumb separator-class="el-icon-arrow-right">
+			<el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+			<el-breadcrumb-item>用户管理</el-breadcrumb-item>
+			<el-breadcrumb-item>用户列表</el-breadcrumb-item>
+		</el-breadcrumb>
+		<!-- 搜索 -->
+		<el-row class="searchRow">
+			<el-col :span="24">
+				<el-input placeholder="请输入内容" class="searchInput">
+					<el-button slot="append" icon="el-icon-search"></el-button>
+				</el-input>
+				<el-button type="success" plain>成功按钮</el-button>
+			</el-col>
+		</el-row>
+	</el-card>
 </template>
 
 <script>
 	export default {
-		
+
 	}
 </script>
 
 <style>
+	.card {
+		height: 100%;
+	}
+	
+	.searchInput {
+		width: 300px;
+	}
+	
+	.searchRow {
+		margin-top: 10px;
+		margin-bottom: 10px;
+	}
 </style>
