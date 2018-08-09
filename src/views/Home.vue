50b5cb1841bc099bd178b17c17f7b9d3c7df85ab
zhanglingqi <504024956@qq.com>
2018-08-09 23:33:13 +0800

    导航菜单的演示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,7 +19,44 @@
 			</el-row>
 		</el-header>
 		<el-container>
-			<el-aside class="aside" width="200px">Aside</el-aside>
+			<!--<el-aside class="aside" width="200px">Aside</el-aside>-->
+			<!--侧边栏-->
+			<el-aside class="aside" width="200px">
+				<!-- default-active 可以让某一个menu-item选中，值是menu-item的index的值 -->
+				<el-menu default-active="1-3">
+					<el-submenu index="1">
+						<template slot="title">
+							<i class="el-icon-location"></i>
+							<span>导航一</span>
+						</template>
+						<el-menu-item-group>
+							<template slot="title">分组一</template>
+							<el-menu-item index="1-1">选项1</el-menu-item>
+							<el-menu-item index="1-2">选项2</el-menu-item>
+						</el-menu-item-group>
+						<el-menu-item-group title="分组2">
+							<el-menu-item index="1-3">选项3</el-menu-item>
+						</el-menu-item-group>
+						<el-submenu index="1-4">
+							<template slot="title">选项4</template>
+							<el-menu-item index="1-4-1">选项1</el-menu-item>
+						</el-submenu>
+					</el-submenu>
+					<el-menu-item index="2">
+						<i class="el-icon-menu"></i>
+						<span slot="title">导航二</span>
+					</el-menu-item>
+					<el-menu-item index="3" disabled>
+						<i class="el-icon-document"></i>
+						<span slot="title">导航三</span>
+					</el-menu-item>
+					<el-menu-item index="4">
+						<i class="el-icon-setting"></i>
+						<span slot="title">导航四</span>
+					</el-menu-item>
+				</el-menu>
+			</el-aside>
+
 			<el-main class="main">Main</el-main>
 		</el-container>
 	</el-container>
